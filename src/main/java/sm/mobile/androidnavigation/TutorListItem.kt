537c6760112d7a30c90d35e116f282eae241ef46
safiramikhareta <136241512+safiramikhareta@users.noreply.github.com>
2025-06-20 11:31:27 +0700

    Delete src directory

--- src/main/java/sm/mobile/androidnavigation/TutorListItem.kt
+++
@@ -1,9 +0,0 @@
-package sm.mobile.androidnavigation
-
-data class TutorListItem (
-    val id: String,
-    val name: String,
-    val imageRes: Int,
-    val subject: String,
-    val rating: Float
-)
--- src/main/java/sm/mobile/androidnavigation/Warna.kt
+++
@@ -1,7 +0,0 @@
-package sm.mobile.androidnavigation
-
-import androidx.compose.ui.graphics.Color
-
-val DeepBlue = Color(0xFF0A3759)
-val LightGrayCard = Color(0xFFF6F6F6)
-val YellowStar = Color(0xFFFFD700)
